7b96245eb075dc265631cb601113cfdf924b1d5a
Taner Halicioglu <taner@taner.net>
2019-07-16 22:48:07 -0700

    tabspacemadness

--- mlat/server/jsonclient.py
+++ mlat/server/jsonclient.py
@@ -314,7 +314,7 @@ class JsonClient(connection.Connection):
                 # Make sure the user string is sane...
                 good_user_regex = '^[A-Za-z0-9_.-]+$'
                 user_ok = re.match(good_user_regex, user)
-		safe_user = re.replace('\n', user)
+                safe_user = re.replace('\n', user)
                 if user_ok is None:
                     raise ValueError("Bad username '{user}'.  Please only use alphanum, '_', '-', or '.'".format(user=safe_user))
 
