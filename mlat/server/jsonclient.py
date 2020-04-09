986e5390fa87f0a30da6f1fe10b21676d57a721c
root <root@mlat-e.adsbexchange.com>
2020-04-09 22:33:29 +0000

    reset sync on receiver clock jump

--- mlat/server/jsonclient.py
+++ mlat/server/jsonclient.py
@@ -604,6 +604,9 @@ class JsonClient(connection.Connection):
             self.process_input_disconnected_message(msg['input_disconnected'])
         elif 'clock_reset' in msg:
             self.process_clock_reset_message(msg['clock_reset'])
+        elif 'clock_jump' in msg:
+            self.coordinator.receiver_clock_reset(self.receiver)
+            self.logger.info("Clock jump, resetting SYNC!")
         elif 'heartbeat' in msg:
             self.process_heartbeat_message(msg['heartbeat'])
         elif 'rate_report' in msg:
