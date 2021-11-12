64a9334a0db74407b7d53186e9bb6bfe90180bab
Matthias Wirth <matthias.wirth@gmail.com>
2021-11-12 16:54:20 +0100

    remove old Cython stuff or py files

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
+import os
+
+# remove potential bogus so files
+pyFiles = [s[:-3] for s in os.listdir('mlat') if s[-2:] == 'py']
+removeFiles = [s for s in os.listdir('mlat') if s[-2:] != 'py' and s.split('.')[0] in pyFiles]
+for f in removeFiles:
+    os.remove('mlat/' + f)
+
 extensions = [
     Extension("mlat.clocksync", ["mlat/clocksync.pyx"]),
     Extension("mlat.clocknorm", ["mlat/clocknorm.pyx"]),
