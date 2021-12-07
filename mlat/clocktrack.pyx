3dac29d4247da57641ba4f9bdf1b57bda2369cb3
Matthias Wirth <matthias.wirth@gmail.com>
2021-12-07 16:19:06 +0100

    test increased sync limits

--- mlat/clocktrack.pyx
+++ mlat/clocktrack.pyx
@@ -78,13 +78,13 @@ cdef double ecef_distance(p0, p1):
 
 cdef get_limit(int cat):
     if cat == 0:
-        return 32
+        return 40
     if cat == 1:
-        return 32
+        return 40
     if cat == 2:
-        return 8
+        return 20
     if cat == 3:
-        return 8
+        return 20
 
 cdef _add_to_existing_syncpoint(clock_pairs, syncpoint, r0, double t0A, double t0B):
     # add a new receiver and timestamps to an existing syncpoint
